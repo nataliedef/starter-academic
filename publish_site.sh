125ada394c5f7785a0b937af4672479231e278f0
nataliedef <ndeforest@natalie-macbook-air.local.dynamic.ucsd.edu>
2021-03-10 11:57:31 -0800

    "Add publish script"

---
+++ publish_site.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## Push website changes to github, and publish to website
+
+### ---------- Usage ----------------
+# ./publish_site.sh 
+# must be in directory that has website
+
+echo "Enter commit message:"
+read message
+
+message="\"$message\""
+echo Pushing changes to website repository...
+git add .
+git commit -m "$message"
+git push -u origin main
+
+# echo Publishing website...
+# hugo
+# cd public
+# git add .
+# git commit -m "Build website"
+# git push origin main
+
